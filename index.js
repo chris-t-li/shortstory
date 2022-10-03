7c70781449a744265f90b1946db4bfdef8837fc4
Chris Li <chris.lty07@gmail.com>
2022-10-03 17:17:41 -0400

    Merge Expected

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Type something here 
+
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eveniet modi dolorem facere aliquam, ea omnis atque eaque facilis, sed laborum laudantium exercitationem rem minima ipsum itaque ipsa odit illum!
+
+// Some other stuff
+
+console.log("hi")
